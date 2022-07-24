fefb8736791d2ca42ff233057ff11fd4e4a7e42d
sachin kumar <sachinkumarshera@gmail.com>
2022-07-25 05:18:54 +0530

    login error fix

--- public/javascripts/accounts/login.js
+++ public/javascripts/accounts/login.js
@@ -1,79 +1,77 @@
 $(document).ready(function () {
-    window.onload = function () {
-        $("form.login-form").submit(function (event) {
-            event.preventDefault();
-            let remember = $("input.remember-me-checked");
-            let isChecked = $(remember).prop("checked");
-            let email = $("form input[type='email'").val();
-            let password = $("form input[type='password']").val();
-            const userData = {
-                "email": email,
-                "password": password
-            }
-            $.ajax({
-                type: "POST",
-                url: atob(decodeURIComponent(getCookie("api"))) + "/authenticate/email-sign-in",
-                processData: false,
-                contentType: "application/json",
-                data: JSON.stringify(userData),
-                beforeSend: function () {
-                    $(".login-btn").prop("disabled", true);
-                    $(".login-btn").html("please wait ...");
-                },
-                success: function (data) {
-                    $(".login-btn").html("Login");
-                    $(".login-btn").prop("disabled", false);
-                    const authToken = data.token;
-                    if (isChecked) {
-                        setCookie("token", authToken, 100);
-                    } else {
-                        setCookie("token", authToken, 1);
-                    }
+    $("form.login-form").submit(function (event) {
+        event.preventDefault();
+        let remember = $("input.remember-me-checked");
+        let isChecked = $(remember).prop("checked");
+        let email = $("form input[type='email'").val();
+        let password = $("form input[type='password']").val();
+        const userData = {
+            "email": email,
+            "password": password
+        }
+        $.ajax({
+            type: "POST",
+            url: atob(decodeURIComponent(getCookie("api"))) + "/authenticate/email-sign-in",
+            processData: false,
+            contentType: "application/json",
+            data: JSON.stringify(userData),
+            beforeSend: function () {
+                $(".login-btn").prop("disabled", true);
+                $(".login-btn").html("please wait ...");
+            },
+            success: function (data) {
+                $(".login-btn").html("Login");
+                $(".login-btn").prop("disabled", false);
+                const authToken = data.token;
+                if (isChecked) {
+                    setCookie("token", authToken, 100);
+                } else {
+                    setCookie("token", authToken, 1);
+                }
+                new Noty({
+                    theme: "nest",
+                    type: "success",
+                    text: '<i class="fa fa-check-circle">  </i>  Login Successful',
+                    timeout: 3000,
+                }).show();
+                window.location = "/dashboard";
+            },
+            complete: function (data) {
+                if (data.status == 400) {
                     new Noty({
                         theme: "nest",
-                        type: "success",
-                        text: '<i class="fa fa-check-circle">  </i>  Login Successful',
+                        type: "error",
+                        text: '<i class="fa fa-check-circle">  </i> Incorrect email address or password',
                         timeout: 3000,
+                        closeWith: ['click', 'button'],
                     }).show();
-                    window.location = "/dashboard";
-                },
-                complete: function (data) {
-                    if (data.status == 400) {
-                        new Noty({
-                            theme: "nest",
-                            type: "error",
-                            text: '<i class="fa fa-check-circle">  </i> Incorrect email address or password',
-                            timeout: 3000,
-                            closeWith: ['click', 'button'],
-                        }).show();
-                        $(".login-btn").html("Login");
-                        $(".login-btn").prop("disabled", false);
-                    }
-                    if (data.status == 0) {
-                        new Noty({
-                            theme: "nest",
-                            type: "error",
-                            text: '<i class="fa fa-check-circle">  </i> Failed to connect server , please try again',
-                            timeout: 3000,
-                            closeWith: ['click', 'button'],
-                        }).show();
-                        $(".login-btn").html("Login");
-                        $(".login-btn").prop("disabled", false);
-                    } else {
-                        new Noty({
-                            theme: "nest",
-                            type: "error",
-                            text: 'Server error - please try again later',
-                            timeout: 3000,
-                            closeWith: ['click', 'button'],
-                        }).show();
-                        $(".login-btn").html("Login");
-                        $(".login-btn").prop("disabled", false);
-                    }
+                    $(".login-btn").html("Login");
+                    $(".login-btn").prop("disabled", false);
                 }
-            })
-        });
-    }
+                if (data.status == 0) {
+                    new Noty({
+                        theme: "nest",
+                        type: "error",
+                        text: '<i class="fa fa-check-circle">  </i> Failed to connect server , please try again',
+                        timeout: 3000,
+                        closeWith: ['click', 'button'],
+                    }).show();
+                    $(".login-btn").html("Login");
+                    $(".login-btn").prop("disabled", false);
+                } else {
+                    new Noty({
+                        theme: "nest",
+                        type: "error",
+                        text: 'Server error - please try again later',
+                        timeout: 3000,
+                        closeWith: ['click', 'button'],
+                    }).show();
+                    $(".login-btn").html("Login");
+                    $(".login-btn").prop("disabled", false);
+                }
+            }
+        })
+    });
 
     if (getCookie("token") !== undefined) {
 
