bc9f45ed550f8dd4537836c43e79bfe1d77c324a
= <=>
2022-09-10 17:29:47 +0530

    universal ads  unit added in homepage

--- public/javascripts/subpages/index.js
+++ public/javascripts/subpages/index.js
@@ -47,14 +47,16 @@ $(document).ready(function () {
                 let adshow = 0;
                 for (let i = 0; i < data.requested.length; i++) {
                     adshow++;
-                    if(adshow == 3){
+                    if(adshow == 4){
                         adshow = 0;
                         $(".most-viewd-notes .row").append(`
-                        <div class="col-lg-4 col-sm-6 mt-3">
+                        <div class="col-lg-4 col-sm-6 mt-3 d-flex justify-content-center align-items-center">
                         <ins class="adsbygoogle"
-                        style="display:inline-block;width:250px;height:250px"
+                        style="display:block"
                         data-ad-client="ca-pub-3834928493837917"
-                        data-ad-slot="1394357315"></ins>
+                        data-ad-slot="5066912970"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
                         </div>`);
                         (adsbygoogle = window.adsbygoogle || []).push({});
                     }
@@ -84,123 +86,6 @@ $(document).ready(function () {
 
         }
     })
-
-
-    // function mostViewed() {
-    //     let hit = false;
-    //     let count = 0;
-    //     $(window).scroll(function (event) {
-    //         let visibility = $(".most-viewd-notes").css("visibility");
-    //         if (!hit) {
-    //             if (visibility !== "hidden") {
-    //                 hit = true;
-    //                 $.ajax({
-    //                     type: "GET",
-    //                     url: atob(getCookie("api")) + "/products/top-performing-products",
-    //                     beforeSend: function () { },
-    //                     success: function (data) {
-    //                         $(".most-viewd-notes .row").html("");
-    //                         if (data.size == 0) {
-    //                             $("section.most-viewd-notes").addClass("d-none");
-    //                         } else {
-
-    //                             for (let i = 0; i < data.requested.length; i++) {
-    //                                 let product_id = data.requested[i].uuid;
-    //                                 let name = data.requested[i].name;
-    //                                 let pages = data.requested[i].pages;
-    //                                 let thumbnails = data.requested[i].thumbnails;
-    //                                 let views = data.requested[i].views;
-    //                                 let timestamp = getTime(data.requested[i].timestamp);
-    //                                 let content = `<div class="col-lg-4 col-sm-6 mt-3"><a href="/notes/${product_id}">
-    //                             <div class="card shadow border-0 h-100">
-    //                                 <div class="card-header">  ${name}</div>
-    //                                 <div class="card-body p-0"><img class="card-img-top" src="${thumbnails.split(",")[0].replace("https://thumbnails.ncdn.in", "https://thumbnails.ncdn.in/fit-in/720x250/filters:format(webp)/filters:quality(100)")}" /></div>
-    //                                 <div class="card-footer">
-    //                                     <div class="notes-cont-info d-flex justify-content-between text-muted mt-2"><small><i class="fa fa-globe"> </i><span> ${views} Views</span></small><small><i class="fa fa-file"> </i><span> ${pages} pages </span></small><small><i class="fa fa-clock"></i><span> ${timestamp} </span></small></div>
-    //                                 </div>
-    //                             </div>
-    //                         </a></div>`;
-
-    //                                 if (count < 6) {
-    //                                     count++;
-    //                                     $(".most-viewd-notes .row").append(content);
-    //                                 }
-    //                             }
-
-    //                         }
-    //                     },
-    //                     error: function () {
-    //                         $(".most-viewd-notes .row").html("");
-    //                         $("section.most-viewd-notes").addClass("d-none");
-
-    //                     }
-    //                 })
-    //             }
-    //         }
-    //     })
-    // };
-
-    // function recentNotes() {
-    //     let hit = false;
-    //     let count = 0;
-    //     $(window).scroll(function (event) {
-    //         let visibility = $(".recent-notes").css("visibility");
-    //         if (!hit) {
-    //             if (visibility !== "hidden") {
-    //                 hit = true;
-    //                 $.ajax({
-    //                     type: "GET",
-    //                     url: atob(getCookie("api")) + "/products/feeds/0",
-    //                     beforeSend: function () { },
-    //                     success: function (data) {
-    //                         $(".recent-notes .row").html("");
-    //                         if (data.requested.length <= 0) {
-    //                             $(".section.recent-notes").removeClass("d-none");
-    //                         } else {
-    //                             for (let i = 0; i < data.requested.length; i++) {
-    //                                 let product_id = data.requested[i].uuid;
-    //                                 let name = data.requested[i].name;
-    //                                 let pages = data.requested[i].pages;
-    //                                 let thumbnails = data.requested[i].thumbnails;
-    //                                 let views = data.requested[i].views;
-    //                                 let timestamp = getTime(data.requested[i].timestamp);
-    //                                 let mainthumbail = thumbnails.split(",")[0].replace("https://thumbnails.ncdn.in", "https://thumbnails.ncdn.in/fit-in/720x250/filters:format(webp)/filters:quality(100)");
-    //                                 var img1 = thumbnails.split(",")[0].replace("https://thumbnails.ncdn.in", "https://thumbnails.ncdn.in/fit-in/320x240/filters:format(webp)/filters:quality(100)");
-
-    //                                 var img2 = thumbnails.split(",")[0].replace("https://thumbnails.ncdn.in", "https://thumbnails.ncdn.in/fit-in/480x360/filters:format(webp)/filters:quality(100)");
-
-    //                                 var img3 = thumbnails.split(",")[0].replace("https://thumbnails.ncdn.in", "https://thumbnails.ncdn.in/fit-in/800x480/filters:format(webp)/filters:quality(100)");
-
-
-    //                                 let content = `<div class="col-lg-4 col-sm-6 mt-3"><a href="/notes/${product_id}">
-    //                             <div class="card shadow border-0 h-100">
-    //                                 <div class="card-header">  ${name}</div>
-    //                                 <div class="card-body p-0"><img class="card-img-top lozad" loading="lazy"  src="${mainthumbail}"  srcset="${img1} 320w,${img2} 480w,${img3} 800w",sizes="(max-width: 320px) 280px,(max-width: 480px) 440px,800px" /></div>
-    //                                 <div class="card-footer">
-    //                                     <div class="notes-cont-info d-flex justify-content-between text-muted mt-2"><small><i class="fa fa-globe"> </i><span> ${views} Views</span></small><small><i class="fa fa-file"> </i><span> ${pages} pages </span></small><small><i class="fa fa-clock"></i><span> ${timestamp} </span></small></div>
-    //                                 </div>
-    //                             </div>
-    //                         </a></div>`;
-
-    //                                 if (count < 6) {
-    //                                     count++;
-    //                                     $(".recent-notes .row").append(content);
-    //                                 }
-    //                             }
-    //                         }
-    //                     },
-    //                     error: function () {
-    //                         $(".recent-notes .row").html("");
-    //                         $(".section.recent-notes").addClass("d-none");
-    //                     }
-    //                 })
-    //             }
-    //         }
-    //     })
-    // }
-
-    // recentNotes();
-
     $.ajax({
         type: "GET",
         url: app.getApi() + "/products/feeds/0",
@@ -217,11 +102,14 @@ $(document).ready(function () {
                     if(adshow == 3){
                         adshow = 0;
                         $(".recent-notes .row").append(`
-                        <div class="col-lg-4 col-sm-6 mt-3">
+                        <div class="col-lg-4 col-sm-6 mt-3 d-flex justify-content-center align-items-center">
                         <ins class="adsbygoogle"
-                        style="display:inline-block;width:250px;height:250px"
+                        style="display:block"
                         data-ad-client="ca-pub-3834928493837917"
-                        data-ad-slot="1394357315"></ins>
+                        data-ad-slot="5066912970"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+
                         </div>`);
                         (adsbygoogle = window.adsbygoogle || []).push({});
                     }
