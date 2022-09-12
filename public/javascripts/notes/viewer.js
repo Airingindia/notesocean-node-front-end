227cfda9fc1053fa3c9cc1feb8283eb1a7dc0900
= <=>
2022-09-12 23:43:42 +0530

    ads test

--- public/javascripts/notes/viewer.js
+++ public/javascripts/notes/viewer.js
@@ -1,32 +1,30 @@
-$(document).ready(function() {
-    document.addEventListener("adobe_dc_view_sdk.ready", function () {
-        var adobeDCView = new AdobeDC.View({ clientId: document.querySelector("#pdfShow").getAttribute("data-client"), divId: "pdfShow" });
-        let resultPromise  = adobeDCView.previewFile(
-            {
-                content: { location: { url: document.querySelector("#pdfShow").getAttribute("data-url") } },
-                metaData: { fileName: document.querySelector(" #pdfShow").getAttribute("data-name") }
-            },
-            {
-                embedMode: "SIZED_CONTAINER",
-                showDownloadPDF: false,
-                showPrintPDF: false,
-                enableLinearization: true,
-                showAnnotationTools: false,
-                enableFormFilling: false,
-                hasReadOnlyAccess: true
-            });
-            resultPromise.then(adobeViewer => {
-                adobeViewer.getAPIs().then(apis => {
-                  apis.getPDFMetadata()
-                  .then(result => {
-                    totalPages = result.numPages;
-                    $(".ads-modal").modal("hide");
-                  })
-                  .catch(error => $(".ads-modal").modal("hide"));
-                });
-              });
+var myModal = new bootstrap.Modal(document.getElementById('adsModal'))
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    var adobeDCView = new AdobeDC.View({ clientId: document.querySelector("#pdfShow").getAttribute("data-client"), divId: "pdfShow" });
+    let resultPromise  = adobeDCView.previewFile(
+        {
+            content: { location: { url: document.querySelector("#pdfShow").getAttribute("data-url") } },
+            metaData: { fileName: document.querySelector(" #pdfShow").getAttribute("data-name") }
+        },
+        {
+            embedMode: "SIZED_CONTAINER",
+            showDownloadPDF: false,
+            showPrintPDF: false,
+            enableLinearization: true,
+            showAnnotationTools: false,
+            enableFormFilling: false,
+            hasReadOnlyAccess: true
         });
-    
-    $(".ads-modal").modal("show");
+        resultPromise.then(adobeViewer => {
+            adobeViewer.getAPIs().then(apis => {
+              apis.getPDFMetadata()
+              .then(result => {
+                totalPages = result.numPages;
+                myModal.hide();
+              })
+              .catch(error =>  myModal.hide());
+            });
+          });
+    });
+    myModal.show();
     (adsbygoogle = window.adsbygoogle || []).push({});
-})
