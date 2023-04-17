9548a8e2e5f46bc93bf4f5ef2c983b2ae52c6dad
Aakash Sharma <aakashvats2910@gmail.com>
2023-04-17 23:56:10 +0530

    signup google button logic added

--- public/javascripts/accounts/signup.js
+++ public/javascripts/accounts/signup.js
@@ -27,6 +27,45 @@ $(document).ready(function () {
         }
     });
 
+    function oauthSignIn() {
+        let client_id =  "668870100811-hsad60ke75oidb448jsiortuv7ols51j.apps.googleusercontent.com"
+        let secret =    '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';
+        let redirect_uri = 'https://dev.notesocean.com/login';
+
+        // Google's OAuth 2.0 endpoint for requesting an access token
+        var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+      
+        // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+        var form = document.createElement('form');
+        form.setAttribute('method', 'GET'); // Send as a GET request.
+        form.setAttribute('action', oauth2Endpoint);
+      
+        // Parameters to pass to OAuth 2.0 endpoint.
+        var params = {'client_id': `${client_id}`,
+                      'redirect_uri': `${redirect_uri}`,
+                      'response_type': 'token',
+                      'scope': 'https://www.googleapis.com/auth/plus.login  https://www.googleapis.com/auth/userinfo.email',
+                      'include_granted_scopes': 'true',
+                      'state': 'pass-through value'};
+      
+        // Add form parameters as hidden input values.
+        for (var p in params) {
+          var input = document.createElement('input');
+          input.setAttribute('type', 'hidden');
+          input.setAttribute('name', p);
+          input.setAttribute('value', params[p]);
+          form.appendChild(input);
+        }
+      
+        // Add form to page and submit it to open the OAuth 2.0 endpoint.
+        document.body.appendChild(form);
+        form.submit();
+    }
+
+    $('.google-sign-in-container').click(function(){
+        oauthSignIn()
+    })
+
 
     $("form.signup-form").submit(function (event) {
         event.preventDefault();
